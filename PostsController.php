7fbf9f28c408aebc12aafe33816340aa7566577f
Brad Pitt <brad@example.com>
2022-12-26 23:55:10 +0800

    modify posts controller

--- PostsController.php
+++ PostsController.php
@@ -4,11 +4,15 @@ class PostsController
 {
     public function inde()
     {
-        // 
+        $posts = Post::all();
+
+        return view('posts.index', compact('posts'));
     }
 
     public function show($id)
     {
-        // 
+        $post = Post::find($id);
+
+        return view('posts.show', compact('post'));
     }
 }
