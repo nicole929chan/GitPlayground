6ae1e199c79bd679fb23ab8c0d4bd830de7c3a85
Brad Pitt <brad@example.com>
2022-12-26 23:49:40 +0800

    add post views

---
+++ views/posts/index.blade.php
@@ -0,0 +1,12 @@
+<div>
+    <h1>Post List</h1>
+    <article>
+        one
+    </article>
+    <article>
+        two
+    </article>
+    <article>
+        three
+    </article>
+</div>
---
+++ views/posts/show.blade.php
@@ -0,0 +1,6 @@
+<div>
+    <h1>Show a post</h1>
+    <article>
+        paragraph one two three
+    </article>
+</div>
